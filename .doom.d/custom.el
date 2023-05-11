fd71ce8f9659e4ef307483f02465fcbd944dc403
j624364 <49117678+j624364@users.noreply.github.com>
2023-05-12 00:18:36 +0100

    Get org agenda filling in properly

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -5,6 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
+ '(org-agenda-files
+   '("~/org/projects/emacs/tests.org" "/home/joe/org/README.org" "/home/joe/org/journal.org"))
  '(package-selected-packages '(exwm-x)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
