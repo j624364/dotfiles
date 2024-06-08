69ec192570c9110b274506b625885be706a65fb9
j624364 <49117678+j624364@users.noreply.github.com>
2024-06-08 15:19:29 +0100

    Make update more automatic

--- Scripts/FullUpdate.sh
+++ Scripts/FullUpdate.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy archlinux-keyring && \
+sudo pacman -Sy archlinux-keyring --noconfirm && \
   sudo pacman-key --populate archlinux && \
   sudo pacman-key --refresh-keys && \
-  sudo pacman -Syu && \
+  sudo pacman -Syu --noconfirm && \
   yay -Syu
 
