2fa621375700f5a451a20a21d0e158c138976d4f
j624364 <49117678+j624364@users.noreply.github.com>
2025-02-12 20:32:34 +0000

    Remove confirm from UpdateKeys.sh

--- Scripts/update/UpdateKeys.sh
+++ Scripts/update/UpdateKeys.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy archlinux-keyring && \
+sudo pacman -Sy archlinux-keyring --noconfirm && \
   sudo pacman-key --populate archlinux && \
   sudo pacman-key --refresh-keys
 
